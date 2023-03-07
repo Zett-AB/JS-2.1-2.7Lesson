4cf716303ebb64ed9ad5b1d5e0227ea1e6627fa1
Alexander <formysite.ru@yandex.ru>
2023-03-07 18:04:50 +0300

    break

---
+++ script.js
@@ -0,0 +1,15 @@
+'use striсt';
+
+//console.log(4/0);
+
+// let persone = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+// console.log(persone["name"]);
+
+// let arr = ['plum.png','orange.jpg','apple.bmp'];
+// console.log(arr[0]);
+
+alert('hello word!');
